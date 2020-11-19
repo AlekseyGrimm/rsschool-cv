b27fe9b25da2fc87817cb89d1d0e6350a2ea6e65
AlekseyGrimm <stalpetc@gmail.com>
2020-11-19 16:44:26 +0300

    fix: fix bag

--- script.js
+++ script.js
@@ -1,62 +1,173 @@
-const burgerButton = document.querySelector('.header__burger');
-const navigation = document.querySelector('.navigation');
-const separators = document.querySelectorAll('.navigation__separator');
-const navigationItems = document.querySelectorAll('.navigation__item');
-const logo = document.querySelector('.header__logo');
-const shadow = document.querySelector('.header__shadow');
+/*Burger Menu*/
+const burgerButton = document.querySelector('#burgerImage');
+const burgerMenu = document.querySelector("#header");
+const body = document.querySelector('#body');
+const BurgerClose = document.querySelector(".burger-burger");
+const homeBurger = document.querySelector('#homeBurger');
+const servicesBurger = document.querySelector('#servicesBurger');
+const portfolioBurger = document.querySelector('#portfolioBurger');
+const aboutBurger = document.querySelector('#aboutBurger');
+const contactBurger = document.querySelector('#contactBurger');
 
 burgerButton.addEventListener('click', () => {
-  burgerButton.classList.toggle('header__burger--active');
-  burgerButton.classList.toggle('header__burger--not-active');
-  navigation.classList.toggle('navigation--mobile');
-  logo.classList.toggle('header__logo--active');
-
-  for (const element of separators) {
-    element.style.display = 'none';
-  }
-  for (const element of navigationItems) {
-    element.style.display = 'block';
-  }
+  burgerMenu.classList.toggle("active");
+  body.classList.add('active');
+});
+ 
+BurgerClose.addEventListener('click', () => {
+  burgerMenu.classList.remove('active');
+  body.classList.remove('active');
 });
 
-for (const element of navigationLinks) {
-  element.addEventListener('click', () => {
-    burgerButton.classList.remove('header__burger--active');
-    burgerButton.classList.add('header__burger--not-active');
-    navigation.classList.remove('navigation--mobile');
-    logo.classList.remove('header__logo--active');
+homeBurger.addEventListener('click', () => {
+  burgerMenu.classList.remove('active');
+  body.classList.remove('active');
+    
+});
+
+servicesBurger.addEventListener('click', () => {
+  burgerMenu.classList.remove('active');
+  body.classList.remove('active');
+});
+  
+portfolioBurger.addEventListener('click', () => {
+  burgerMenu.classList.remove('active');
+  body.classList.remove('active');
+});
+
+aboutBurger.addEventListener('click', () => {
+  burgerMenu.classList.remove('active');
+  body.classList.remove('active');
+});
+  
+contactBurger.addEventListener('click', () => {
+  burgerMenu.classList.remove('active');
+  body.classList.remove('active');
+});
+
+ 
+
+/*Navigation Color*/
+  const homeHeader = document.querySelector('#homeHeader');
+  const servicesHeader = document.querySelector('#servicesHeader');
+  const portfolioHeader = document.querySelector('#portfolioHeader');
+  const aboutHeader = document.querySelector('#aboutHeader');
+  const contactHeader = document.querySelector('#contactHeader') ;
+
+
+  homeHeader.addEventListener('click', () => {
+    resetChangeColor();
+    homeHeader.classList.add('changeColor');
   });
-}
 
-const home = document.querySelector('.header');
-const services = document.querySelector('.services');
-const portfolio = document.querySelector('.portfolio');
+  servicesHeader.addEventListener('click', () => {
+    resetChangeColor();
+    servicesHeader.classList.add('changeColor');
+  });
 
-const homePositionY = home.getBoundingClientRect().y + window.pageYOffset;
-const servicesPositionY = services.getBoundingClientRect().y + window.pageYOffset;
-const portfolioPositionY = portfolio.getBoundingClientRect().y + window.pageYOffset;
+  portfolioHeader.addEventListener('click', () => {
+    resetChangeColor();
+    portfolioHeader.classList.add('changeColor');
+  });
 
-const homeLink = document.getElementById('homeId');
-const servicesLink = document.getElementById('servicesId');
-const portfolioLink = document.getElementById('portfolioId');
+  aboutHeader.addEventListener('click', () => {
+    resetChangeColor();
+    aboutHeader.classList.add('changeColor');
+  });
 
-window.addEventListener('scroll', () => {
-  if (window.pageYOffset >= homePositionY) {
-    for (let element of navigationLinks) {
-      element.classList.remove('navigation__link--active');
-    }
-    homeLink.classList.add('navigation__link--active');
-  }
-  if (window.pageYOffset >= servicesPositionY) {
-    for (let element of navigationLinks) {
-      element.classList.remove('navigation__link--active');
-    }
-    servicesLink.classList.add('navigation__link--active');
-  }
-  if (window.pageYOffset >= portfolioPositionY) {
-    for (let element of navigationLinks) {
-      element.classList.remove('navigation__link--active');
-    }
-    portfolioLink.classList.add('navigation__link--active');
-  }
-})
+  contactHeader.addEventListener('click', () => {
+    resetChangeColor();
+    contactHeader.classList.add('changeColor');
+});
+
+  
+    // Scroll
+    const home = document.getElementById("homeHeader");
+    const services = document.getElementById("servicesHeader");
+    const portfolio = document.getElementById("portfolioHeader");
+    const about = document.getElementById("aboutHeader");
+    const contact = document.getElementById("contactHeader");
+ 
+      window.addEventListener("scroll", () => {
+        if (window.pageYOffset >= 0 && window.pageYOffset < 365) {
+          home.classList.add("changeColor");
+          services.classList.remove("changeColor");
+          portfolio.classList.remove("changeColor");
+          about.classList.remove("changeColor");
+          contact.classList.remove("changeColor");
+        } else if (window.pageYOffset >= 500 && window.pageYOffset < 1000) {
+          services.classList.add("changeColor");
+          home.classList.remove("changeColor");
+          portfolio.classList.remove("changeColor");
+          about.classList.remove("changeColor");
+          contact.classList.remove("changeColor");
+        } else if (window.pageYOffset >= 754) {
+          services.classList.remove("changeColor");
+          home.classList.remove("changeColor");
+          portfolio.classList.add("changeColor");
+          about.classList.remove("changeColor");
+          contact.classList.remove("changeColor");
+        } else {
+          services.classList.remove("changeColor");
+        }
+      });
+  
+      function resetChangeColor() {
+        homeHeader.classList.remove('changeColor');
+        servicesHeader.classList.remove('changeColor');
+        portfolioHeader.classList.remove('changeColor');
+        aboutHeader.classList.remove('changeColor');
+        contactHeader.classList.remove('changeColor');
+
+        tagAll.classList.remove('colorTag');
+        tagWebDesign.classList.remove('colorTag');
+        tagGraphicDesign.classList.remove('colorTag');
+        tagArtWork.classList.remove('colorTag');
+
+        body.classList.remove('active');
+     };
+
+        /*Sort Images*/ 
+        const imagesOne = document.querySelector('#imagesOne');
+        const tagAll = document.querySelector('#tagAll');
+        const tagWebDesign = document.querySelector('#tagWebDesign');
+        const tagGraphicDesign = document.querySelector('#tagGraphicDesign');
+        const tagArtWork = document.querySelector('#tagArtWork');
+
+          tagAll.addEventListener('click', () => {
+            resetChangeColor();
+          tagAll.classList.add('colorTag');
+          imagesOne.classList.toggle('changeImages');
+          });
+
+          tagWebDesign.addEventListener('click', () => {
+            resetChangeColor();
+            tagWebDesign.classList.add('colorTag');
+            imagesOne.classList.toggle('changeImages');
+          });
+
+          tagGraphicDesign.addEventListener('click', () => {
+            resetChangeColor();
+            tagGraphicDesign.classList.add('colorTag');
+            imagesOne.classList.toggle('changeImages');
+          });
+
+          tagArtWork.addEventListener('click', () => {
+            resetChangeColor();
+            tagArtWork.classList.add('colorTag');
+            imagesOne.classList.toggle('changeImages');
+          });
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
