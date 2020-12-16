10954eca67e04b360acab706f93fd16e1a00b2d1
AlekseyGrimm <stalpetc@gmail.com>
2020-12-16 21:41:32 +0300

    feat: update this code

--- JS-Drum-Kit/app.js
+++ JS-Drum-Kit/app.js
@@ -1,5 +1,7 @@
+const keys = document.querySelectorAll('.key');
+
 // Play drum by keyboard press
-window.addEventListener('keydown' , playDrum);
+
 function playDrum(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     if(!audio) return;
@@ -8,25 +10,23 @@ function playDrum(e) {
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
     key.classList.add('playing');
 };
+window.addEventListener('keydown' , playDrum);
+
 
 // Remove .playing class after transition action over
-const keys = document.querySelectorAll('.key');
 keys.forEach( key => key.addEventListener('transitionend' , removePlayingClass ) );
-function removePlayingClass(e)
-{
-    //if( e.propertyName != 'transform' ) return;
+function removePlayingClass(e){
+    if( e.propertyName != 'transform' ) return;
     this.classList.remove('playing');
-}
-
+};
 
 // Play music by mouse click
-keys.forEach( key => key.addEventListener('click' , playByMouse ) );
-function playByMouse(e)
-{
-    const keyCode = this.getAttribute('data-key');
+keys.forEach( key => key.addEventListener('click' , playByMouse ));
+function playByMouse(e){
+ const keyCode = this.getAttribute('data-key');
 	const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
 	audio.currentTime = 0;
 	audio.play();
 	const key = document.querySelector(`.key[data-key="${keyCode}"]`);
     key.classList.add('playing');
-}
+};
