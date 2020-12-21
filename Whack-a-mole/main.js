4b56bea2bedf825cd3c2853267a19eeb93c67ca8
AlekseyGrimm <stalpetc@gmail.com>
2020-12-22 00:15:31 +0300

    feat: update this code

--- Whack-a-mole/main.js
+++ Whack-a-mole/main.js
@@ -1,13 +1,16 @@
 const holes = document.querySelectorAll(".hole");
 const scoreBoard = document.querySelector(".score");
 const moles = document.querySelectorAll(".mole");
+const level1 = document.querySelector(".easy");
+const level2 = document.querySelector(".medium");
+const level3 = document.querySelector(".hard");
 let lastHole;
 let timeUp = false;
 let score = 0;
 
 function randomTime(min, max) {
     return Math.round(Math.random() * (max - min) + min);
-}
+};
 
 function randomHole(holes) {
     let idx = Math.floor(Math.random() * holes.length);
@@ -17,31 +20,50 @@ function randomHole(holes) {
     }
     lastHole = hole;
     return hole;
-}
+};
 
-function peep() {
-    let time = randomTime(200, 1000);
-    let hole = randomHole(holes);
+function peep(a, b) {
+    let time = randomTime(a, b);
+    const hole = randomHole(holes);
     hole.classList.add("up");
     setTimeout(() => {
-        hole.classList.remove("up");
-        if (!timeUp) peep();
+      hole.classList.remove("up");
+      if (!timeUp) peep(a, b);
     }, time);
-}
+  };
 
-function startGame() {
-    scoreBoard.textContent = 0;
-    timeUp = false;
-    score = 0;
-    peep();
-    setTimeout(() => timeUp = true, 10000)
-}
 
 // writes the score
 function bonk(e) {
+    if (!e.isTrusted) return;
     score++;
     this.classList.remove("up");
     scoreBoard.textContent = score;
-}
+    localStorage.setItem("totalScore", scoreBoard.textContent);
+};
+
+function startGame(a, b) {
+    scoreBoard.textContent = 0;
+    timeUp = false;
+    score = 0;
+    peep(a, b);
+    setTimeout(() => (timeUp = true), 10000);
+};
+
+function startLevel1() {
+    startGame(700, 1000);
+};
+  
+function startLevel2() {
+    startGame(400, 700);
+};
+  
+function startLevel3() {
+    startGame(100, 400);
+};
 
 moles.forEach(mole => mole.addEventListener("click", bonk));
+
+level1.addEventListener("click", startLevel1);
+level2.addEventListener("click", startLevel2);
+level3.addEventListener("click", startLevel3);
