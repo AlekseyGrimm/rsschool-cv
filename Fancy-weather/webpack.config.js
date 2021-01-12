d39eb5d1c7f70b4baf7a506f016e79e64ac747e1
AlekseyGrimm <stalpetc@gmail.com>
2021-01-12 16:30:44 +0300

    feat: add webpack

---
+++ Fancy-weather/webpack.config.js
@@ -0,0 +1,42 @@
+let path = require('path');
+let ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+let conf = {
+    entry: './src/index.js',
+    output:{
+        path: path.resolve(__dirname, './dist'),
+        filename: 'main.js',
+        publicPath: 'dick/'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+            },
+            {
+                test:/\.(html)$/,
+                use:['html-loader'],
+            },
+            {
+                test:/\.(?:ico|gif|png|jpg|jpeg|svg)$/i,
+                type: 'asset/resource'
+            },
+            {
+                test:/\.(mp3)$/i,
+                type: 'asset/resource'
+            },
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                  fallback: "style-loader",
+                  use: "css-loader"
+                })
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin("styles.css"),
+      ]
+}
